0948a3d1a691766bfd43d0c73e61b2d6a1628139
Woosang <yws1502@naver.com>
2021-12-29 00:39:20 +0900

    ✨ Create Write page -> Create post function

---
+++ src/pages/write/index.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import {NextPage} from "next";
+import React, {useState} from "react";
+
+const write = (title: string, post: string) => {
+  axios.post("http://localhost:1337/api/posts/", {
+    "data" : {
+      "author" : "woosang",
+      "title" : title,
+      "contents" : post,
+    }
+  }).then((res) => {
+    if (res.status === 200) {
+      location.href = "/posts"
+    }
+  })
+}
+
+const WritePage: NextPage = () => {
+  const [title, setTitle] = useState("");
+  const [post, setPost] = useState("");
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }
+
+  const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPost(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    write(title, post);
+  }
+  
+
+  return (
+    <div>
+      <h1>글 작성</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleTitleChange} value={title} />
+        <textarea onChange={handleTextAreaChange} value={post}/>
+        <button type="submit">발행하기</button>
+      </form>
+    </div>
+  )
+}
+
+export default WritePage;
